cc9735699f4ca7599c600f0420a01bb20c66fae6
NickRest <nickrestemail@gmail.com>
2022-09-20 11:30:54 -0400

    Background

---
+++ mainmenu/index.js
@@ -0,0 +1,19 @@
+const sourceElem = document.querySelector('video');
+const ENDPOINT = "https://assets.sliceclient.com/mainmenubackground/";
+const backgrounds = ["Spaceman.webm", "Zero Two.webm"];
+const urlParams = new URLSearchParams(window.location.search);
+const imageElem = document.querySelector('img');
+const pElem = document.querySelector('p');
+
+function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function setBackground() {
+    sourceElem.src = ENDPOINT + getRandomElement(backgrounds);
+}
+
+setBackground();
+
+imageElem.src = `https://minotar.net/helm/${urlParams.get("name")}/50.png`;
+pElem.innerText = urlParams.get("name");
