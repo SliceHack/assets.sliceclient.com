5ba43e37e671afe991a6c304f7dcae90b346c5fb
NickRest <nickrestemail@gmail.com>
2022-10-09 10:59:33 -0400

    temp comment out background on mainmenu

--- mainmenu/background.js
+++ mainmenu/background.js
@@ -1,19 +1,19 @@
-const sourceElem = document.querySelector('video');
-const ENDPOINT = "https://assets.sliceclient.com/mainmenubackground/";
-const backgrounds = ["Spaceman.webm", "Zero Two.webm", "Touhou - Bad Apple.webm"];
+// const sourceElem = document.querySelector('video');
+// const ENDPOINT = "https://assets.sliceclient.com/mainmenubackground/";
+// const backgrounds = ["Spaceman.webm", "Zero Two.webm", "Touhou - Bad Apple.webm"];
 const urlParams = new URLSearchParams(window.location.search);
 const imageElem = document.querySelector('img');
 const pElem = document.querySelector('p');
 
-function getRandomElement(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
+// function getRandomElement(arr) {
+//     return arr[Math.floor(Math.random() * arr.length)];
+// }
 
-function setBackground() {
-    sourceElem.src = ENDPOINT + getRandomElement(backgrounds);
-}
-
-setBackground();
+// function setBackground() {
+//     sourceElem.src = ENDPOINT + getRandomElement(backgrounds);
+// }
+//
+// setBackground();
 
 imageElem.src = `https://minotar.net/helm/${urlParams.get("name")}/50.png`;
 pElem.innerText = urlParams.get("name");
